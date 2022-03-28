f177fe0872c3d975fdfb1d368cc8975f1eef4dc3
ochnios <szymonochnio15@gmail.com>
2022-03-28 23:24:13 +0200

    added ifndef .. endif

--- src/graph.h
+++ src/graph.h
@@ -1,5 +1,10 @@
+#ifndef _GRAPH_H_
+#define _GRAPH_H_
+
 // stores pairs (weight, node) used in Dijkstra algorithm and priority queue
 typedef struct pair {
     double weight;  // -1 by default
     int node;       // -1 by default
 } pair_t;
+
+#endif
