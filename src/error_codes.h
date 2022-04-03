123a5bddcd7a8b80b448be0459e85a90ae887d3e
ochnios <szymonochnio15@gmail.com>
2022-04-03 22:51:33 +0200

    added header file with error codes

---
+++ src/error_codes.h
@@ -0,0 +1,21 @@
+#ifndef _ERROR_CODES_H_
+#define _ERROR_CODES_H_
+
+/*
+Errors prefixes for modules:
+    1 - main
+    2 - reader
+    3 - splitter
+    4 - writer
+    5 - dijkstra
+    6 - bfs
+    7 - priority queue
+    8 - queue
+    9 - graph
+*/
+
+#define PQ_FAILED_ALLOC     70
+#define PQ_FAILED_REALLOC   71
+#define PQ_IS_NULL          72
+
+#endif
