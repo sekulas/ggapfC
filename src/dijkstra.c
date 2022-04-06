b0e4fd7ad0d83cf4a32b79ec71cfab58da9c2e35
ochnios <szymonochnio15@gmail.com>
2022-04-06 21:27:59 +0200

    fixed dijksra, added backtrace function

--- src/dijkstra.c
+++ src/dijkstra.c
@@ -8,14 +8,61 @@
 #include "reader.h"
 #include "dijkstra.h"
 
+// for tests only
 void print_distances_arr(double * arr, int n) {
     for(int i = 0; i < n; i++)
         printf("%d: %f\n", i, arr[i]);
 }
+void print_prev_arr(int * arr, int n) {
+    for(int i = 0; i < n; i++)
+        printf("%d: %d\n", i, arr[i]);
+}
+
+
+// shows the course of the shortest path
+void backtrace(double * distance, int * prev, int begin, int end) {
+    int current_vertex;
+    int prev_vertex;
+    int i = 0;
+    double weight;
+
+    // show shortest path or info that there is no shortest path
+    if(distance[end] >= FLT_MAX) {
+        printf("Shortest path = INFINITY\nThere is no path between given vertices\n");
+        return;
+    } else 
+        printf("Shortest path = %g\n", distance[end]);
+
+    // show vertices thorugh shortest path leads by
+    // backtracing by prev array
+    current_vertex = end;
+    while(current_vertex != begin) {
+        printf("%d <- ", current_vertex);
+        current_vertex = prev[current_vertex];
+    }
+    printf("%d\n\n", current_vertex);
+
+    // show individual weights between vertices on shortest path
+    // backtracing by prev array
+    printf("Weights of individual edges:\n");
+    current_vertex = end;
+    while(current_vertex != begin) {
+        prev_vertex = prev[current_vertex];
+        weight = distance[current_vertex] - distance[prev_vertex];
+
+        printf("%5d <-%5d  =  %g\n", current_vertex, prev_vertex, weight);
+        current_vertex = prev_vertex;
+        i++;
+    }
+
+    // additional info
+    printf("The shortest path leads through %d vertices\n\n", i);
+}
 
 // finds shortest path betweent given vertices
 // returns shortest path lenght or DBL_MAX when there is no path between given vertices
-double dijkstra(graph_t * graph, int begin, int end) {
+// if show_backtrace != 0, the course of the shortest path will be presented
+double dijkstra(graph_t * graph, int begin, int end, int show_backtrace) {
     
     // ??? checking if begin and end values are correct should happen here or in main.c ?
     
@@ -24,7 +71,7 @@ double dijkstra(graph_t * graph, int begin, int end) {
 
     pair_t current;     // the vertex from which the paths are examined
     pair_t examined;    // the vertex to which examined path leads
-    long double new_distance;    // potential new distance between current and examined
+    double new_distance;    // potential new distance between current and examined
 
     double * distance = malloc(sizeof(double) * graph->nodes);  // currently known shortest paths lengths
     int * seen = malloc(sizeof(int) * graph->nodes);            // for marking examined vertices
@@ -39,51 +86,49 @@ double dijkstra(graph_t * graph, int begin, int end) {
 
     // fill arrays with default values
     for(i = 0; i < graph->nodes; i++) {
-        distance[i] = FLT_MAX;
+        distance[i] = FLT_MAX;  // FLT_MAX should be enough
         seen[i] = UNSEEN;
         prev[i] = PREV_UNKNOWN;
     }
 
     // add pair (0, begin) to pq
     add_to_pq(pq, 0.0, begin);
-
-    // set values for firs node
+    // set 0 distance to begin node
     distance[begin] = 0.0;
-    seen[begin] = SEEN;
-
-    result = DBL_MAX;
+    prev[begin] = begin;
+    // set current node as begin for correct entry into the loop
+    current.node = begin; 
 
     // dijkstras algorithm
-    do {
-        current = get_from_pq(pq);
+    while(!is_empty(pq) && current.node != end) {   // while pq is not empty and end node is not reached
+        current = get_from_pq(pq);                      // take node with the smallest weight from pq
+        seen[current.node] = SEEN;                      // mark current node as seen - current distance to this node is shortest distance to it
         for(i = 0; i < ADJ_LIST_COLS; i++) {            // examine adjacent vertices
-            examined = graph->edge[current.node][i];    // take neighbour of current node
-            if(examined.node == DEFAULT_NODE || seen[examined.node] == SEEN) {    // if there is no connection or node was examined continue 
-                printf("continued!");
-                continue;
-            }
+            examined = graph->edge[current.node][i];        // take neighbour of current node
+            if(examined.node == DEFAULT_NODE || seen[examined.node] == SEEN)
+                continue;   // when there is no connection or shortest path is already known
 
             new_distance = distance[current.node] + examined.weight;    // set potential new distance to examined node
             if(distance[examined.node] > new_distance) {                // check if potential new distance is less than currently known one
-                printf("tests!");
                 distance[examined.node] = new_distance;                     // yes - save new distance to known distances array
-                add_to_pq(pq, new_distance, examined.node);                 // add examined node and its new distance to pq
                 prev[examined.node] = current.node;                         // set previous node through shortest path leads by
-                seen[examined.node] = SEEN;                                 // set flag that examined node was seen
-
-                if(examined.node == end) {
-                    return examined.weight;
-                }
+                add_to_pq(pq, new_distance, examined.node);                 // add examined node and its new distance to pq
             }
-        }
-        printf("\n");
+        }   
+    } 
 
-        current = get_from_pq(pq);      // take node with the smallest weight from pq
-    } while(!is_empty(pq) && current.node != end); // while pq is not empty and end node is not reached
+    // if loop has been completed due to reaching the end vertex - shortest path was found
+    // in other case - there is no connection between given nodes so result is infinity (FLT_MAX)
+    if(current.node == end) result = distance[current.node];
+    else result = DBL_MAX; 
 
-    result = distance[current.node];
-    print_distances_arr(distance, graph->nodes);
+    //print_distances_arr(distance, graph->nodes);
+    //print_prev_arr(prev, graph->nodes);
 
+    // show detailed courese on request
+    if(show_backtrace) backtrace(distance, prev, begin, end);
+
+    // free dynamicly allocated memory
     free(distance);
     free(seen);
     free(prev);
@@ -92,9 +137,10 @@ double dijkstra(graph_t * graph, int begin, int end) {
     return result;
 }
 
+// main is here for tests
 int main(int argc, char **argv) {
     graph_t * graph = read_from_file("data/graph1");
-    show_graph(graph, stdout);
-    printf("shortest path: %g\n", dijkstra(graph, atoi(argv[1]), atoi(argv[2])));
+    //show_graph(graph, stdout);
+    dijkstra(graph, atoi(argv[1]), atoi(argv[2]), 1);
     return 0;
 }
--- src/dijkstra.h
+++ src/dijkstra.h
@@ -8,4 +8,12 @@
 #define UNSEEN          0
 #define PREV_UNKNOWN    -1
 
+// shows the course of the shortest path
+void backtrace(double * distance, int * prev, int begin, int end);
+
+// finds shortest path betweent given vertices
+// returns shortest path lenght or DBL_MAX when there is no path between given vertices
+// if show_backtrace != 0, the course of the shortest path will be presented
+double dijkstra(graph_t * graph, int begin, int end, int show_backtrace);
+
 #endif
