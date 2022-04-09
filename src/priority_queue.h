6c372dd5af6c1e19ed49f94b1b58bcf1f1c891a3
ochnios <szymonochnio15@gmail.com>
2022-04-09 12:14:00 +0200

    little fix

--- src/priority_queue.h
+++ src/priority_queue.h
@@ -19,36 +19,36 @@ typedef struct PQ {
 pq_t * init_pq();
 
 // frees memory used by priority queue
-void free_pq(pq_t *);
+void free_pq(pq_t * pq);
 
 // expands pririty queue - reallocation mechanism
 // returns pointer to reallocated pq on success and error code to shell in other case
-pq_t * expand_pq(pq_t*);
+pq_t * expand_pq(pq_t * pq);
 
 // returns true (1) when pq is empty
-int is_empty(pq_t *);
+int is_empty(pq_t * pq);
 
 // polls and returns pair with the smallest weight from pq (root)
 // returns (-1, -1) when pq is empty
-pair_t get_from_pq(pq_t*);
+pair_t get_from_pq(pq_t * pq);
 
 // adds pair to pq
 // returns 1 on success and error code to shell in other case
-int add_to_pq(pq_t*, double, int);
+int add_to_pq(pq_t * pq, double weight, int node);
 
 // returns true (value > 0) when p1 < p2 and false (0) in other case
-int is_less(pq_t*, int, int);
+int is_less(pq_t * pq, int p1, int p2);
 
 // swaps p1 with p2
-void swapp(pq_t*, int, int);
+void swapp(pq_t * pq, int p1, int p2);
 
 // moves element up to its right place
-void bubble_up(pq_t*);
+void bubble_up(pq_t * pq);
 
 // moves element down to its right place
-void bubble_down(pq_t*);
+void bubble_down(pq_t * pq);
 
 // shows pq
-void show_pq(pq_t*);
+void show_pq(pq_t * pq);
 
 #endif
