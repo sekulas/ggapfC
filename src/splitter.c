f5b21bb231a6fe3b2dec40f596197ff5331a1a0c
sekulas <sekula.sebastian22@gmail.com>
2022-04-11 12:37:21 +0200

    Splitter SetUp

--- src/splitter.c
+++ src/splitter.c
@@ -3,7 +3,25 @@
 
 #include "error_codes.h"
 #include "graph.h"
+#include "splitter.h"
+
 
 void splitter(graph_t * graph) {
     
+}
+
+//returns info about node ( is it on the edge or not )
+int is_on_the_edge(graph_t * graph, int node) {
+
+    int k = 0;  //it's gona count how many connections a node have
+
+    for(int i = 0; i < ADJ_LIST_COLS; i++)
+        if(graph->edge[node][i].node != DEFAULT_NODE)
+            k++;
+        
+    if( k < ADJ_LIST_COLS )
+        return EDGE_NODE;
+    
+    return NOT_EDGE_NODE;
+
 }
--- src/splitter.h
+++ src/splitter.h
@@ -3,8 +3,15 @@
 
 #include "graph.h"
 
-void splitter(graph_t * graph) {
-    
-}
+#define EDGE_NODE       1
+#define NOT_EDGE_NODE   0
+#define SPLIT_MODE      1
+#define NOT_SPLIT_MODE  0
+
+//splits our graphs
+void splitter(graph_t * graph, char * prev);
+
+//returns info about node ( is it on the edge or not )
+int is_on_the_edge(graph_t * graph, int node);
 
 #endif
